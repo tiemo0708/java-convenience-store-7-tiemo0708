541487724fe42c73f5c657f834cd085ebb103c1c
jinhyeon <tiemo0708@naver.com>
2024-11-11 23:04:13 +0900

    test: 테스트코드 수정

--- src/test/java/store/ApplicationTest.java
+++ src/test/java/store/ApplicationTest.java
@@ -56,7 +56,7 @@ class ApplicationTest extends NsTest {
     @Test
     void 예외_테스트() {
         assertSimpleTest(() -> {
-            runException("[컵라면-12]", "N", "N");
+            runException("[컵라면-12]");
             assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
         });
     }
