1540e7c7a7ed56ae7f65c46f1e194ae6a50d3eb3
jinhyeon <tiemo0708@naver.com>
2024-11-11 19:10:55 +0900

    fix: 출력을 양식에 맞게 수정

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
     private static final String PRODUCT_INPUT_MESSAGE = "\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String PROMOTION_MESSAGE = "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n";
     private static final String PROMOTION_INVALID_MESSAGE = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\"\n";
-    private static final String MEMBERSHIP_INVALID_MESSAGE ="멤버십 할인을 받으시겠습니까? (Y/N)";
+    private static final String MEMBERSHIP_INVALID_MESSAGE ="\n멤버십 할인을 받으시겠습니까? (Y/N)";
 
     public String getProductInput() {
         System.out.println(PRODUCT_INPUT_MESSAGE);
